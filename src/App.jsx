12572c01dd4962dd6aa8c51eee880f7d9d154f85
Naki <leandroreisubi@gmail.com>
2024-07-17 22:52:05 -0300

    About me

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Box />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/sections/apresentationPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const apresentationPage = () => {
+    return (
+        <div>
+            <h1>Apresentation page</h1>
+        </div>
+    )
+}
+
+export default apresentationPage
